67cafba4029191918459dbf0cb7b81a90bbf7c7d
Sankranti Joshi <sankrantijoshi14@gmail.com>
2023-01-18 18:41:34 +0000

    More changes and final cnn model plus adding code for Adding orbit dataset

---
+++ src/cv/pytorch/data_loader/use_cases/facial_keypoint_detection/dataloader.py
@@ -0,0 +1,124 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+from skimage import io
+from matplotlib import pyplot as plt
+from torchvision import transforms
+from src.cv.pytorch.data_loader.dataloader import CustomDatasetLoader
+from typing import Optional
+from src.cv.pytorch.data_loader.configs import (
+    CustomDataset,
+)
+from src.cv.pytorch.data_loader.use_cases.facial_keypoint_detection.transforms import (
+    Grayscale,
+    RandomCrop,
+    Resize, 
+    ToTensor
+)
+
+
+class FacialKeypointDataLoader(CustomDatasetLoader):
+
+    def __init__(
+        self, 
+        data_type: str,
+        dataset_name: str, 
+        annotation_file: Optional[str] = None, 
+        data_file: Optional[str] = None,
+        image_column: Optional[str] = None,
+        img_dir: Optional[str] = None, 
+        random_crop_size: int = 224,
+        resize_size: int = 256
+    ) -> None:
+        
+        self.__image_column = image_column
+        if data_type == "image":
+            super().__init__(
+                data_type = data_type,
+                dataset_name=dataset_name, 
+                img_dir=img_dir, 
+                annotation_file=annotation_file, 
+                composed_transforms=transforms.Compose([
+                    Grayscale(data_type=data_type),
+                    Resize(data_type=data_type, size=resize_size),
+                    RandomCrop(random_crop_size),
+                    ToTensor(),
+                ])
+            )
+        
+        else:
+
+            super().__init__(
+                data_type = data_type,
+                dataset_name=dataset_name,
+                data_file=data_file,
+                composed_transforms=transforms.Compose([
+                    Resize(data_type=data_type, size=resize_size),
+                    Grayscale(data_type=data_type)
+                ])
+            )
+
+    def _load_custom_image_data(self, data_file, dataset_name):
+        
+        data = pd.read_csv(data_file)
+        if not self.__image_column:
+            if "Image" not in data.columns:
+                raise ValueError(
+                    "No way to identify column containing image data"
+                    "Please provide value in image_column argument "
+                    "or put image data is 'Image' column"
+                )
+            self.__image_column = "Image"
+
+
+        if not isinstance(data.loc[0, self.__image_column], np.ndarray):
+            data[self.__image_column] = data[self.__image_column].apply(
+                lambda x: np.array(x.split(" "), dtype="float"))
+        data.fillna(method = 'ffill',inplace = True)
+        image_data = data.pop(self.__image_column)
+        self._image_labels = data
+
+        self.dataset = CustomDataset(
+            dataset_name=dataset_name, 
+            image_labels=data, 
+            image_data=image_data
+        )
+
+    def _get_image_data(self, idx):
+
+        # getting image name from keypoint file
+        img_name = os.path.join(self.dataset.img_directory, self.dataset.image_labels.iloc[idx, 0])
+        # reading input file in
+        image = io.imread(img_name)
+        
+        # extracting keypoints for the image
+        image_label = np.array(
+            self.dataset.image_labels.iloc[idx, 1:]).astype("float").reshape(-1, 2)
+        sample = {'image': image, 'facial_landmarks': image_label}
+
+        return sample
+
+    def __getitem__(self, idx):
+        """ 
+        Function copied from https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+        
+        """
+        # get idx to be returned as a list
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        if self._data_type == "image":
+            sample = self._get_image_data(self, idx)
+
+        else:
+            image = self.dataset.image_data[idx]
+            keypoints = np.asarray(
+                self.dataset.image_labels.iloc[idx, :]).astype('float').reshape(-1, 2)
+
+            sample = {"image": image, "facial_landmarks": keypoints}
+
+        sample = self.transform(sample)
+        return sample
+
+    
---
+++ src/cv/pytorch/data_loader/use_cases/facial_keypoint_detection/transforms.py
@@ -0,0 +1,148 @@
+from typing import Dict, Union, Tuple
+import numpy as np
+import cv2
+
+class Grayscale:
+    
+    def __init__(self, data_type: str):
+
+        if data_type not in {"image", "csv"}:
+            raise ValueError(
+                "Arg data_type must be an element of {image, csv}"
+                f"You provided: {data_type}"
+            )
+
+        self._data_type = data_type
+
+    def __call__(self, data: Dict):
+        
+        image = data["image"]
+        key_points = data["facial_landmarks"]
+        if self._data_type == "image":
+            
+            # rescaling [0 to 1] from [0, 255]
+            grayscale_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)/255
+            return {'image': grayscale_image, "facial_landmarks": key_points}
+        
+        else:
+            return {"image": image/255, "facial_landmarks": key_points}
+    
+
+class Resize:
+    
+    def __init__(self, data_type: str, size: Union[int, Tuple[int, int]]):
+
+        
+        if data_type not in {"image", "csv"}:
+            raise ValueError(
+                "Arg data_type must be an element of {image, csv}"
+                f"You provided: {data_type}"
+            )
+
+        self._data_type = data_type
+
+
+        if not isinstance(size, (int, tuple)):
+            raise ("Argument Size takes values of type int or tuple")
+            
+        if isinstance(size, tuple):
+            if len(size) != 2:
+                raise ValueError("Size Tuple should have two elements- height and weight")
+        else:
+            self._size = int(size)
+        
+    def _get_int_based_size_for_larger_dimension(self, bigger_size, small_size) -> int:
+        
+        bigger_size = bigger_size * self._size/small_size
+        
+        return int(bigger_size)
+        
+    def __call__(self, data: Dict):
+        image = data["image"]
+        key_points = data["facial_landmarks"]
+        if self._data_type == "image":
+
+            current_height, current_weight = image.shape
+            
+            if isinstance(self._size, int):
+                
+                if current_height > current_weight:
+                    
+                    new_height = self._get_int_based_size_for_larger_dimension(
+                        bigger_size=current_height, small_size=current_weight)
+                    
+                    new_weight = self._size
+                    
+                else:
+                    new_weight = self._get_int_based_size_for_larger_dimension(
+                        bigger_size=current_weight, small_size=current_height)
+                    
+                    new_height = self._size
+                    
+            else:
+                new_height, new_weight = self._size
+                
+            
+            return {
+                "image": cv2.resize(image, (new_weight, new_height)), 
+                "facial_landmarks": key_points * [new_weight/current_weight, new_height/current_height]
+            }
+            
+        else:
+            if isinstance(self._size, int):
+                image.resize(self._size, self._size, 1)
+            else:
+                image.resize(self._size[0], self._size[1], 1)
+            return {
+                "image": image,
+                "facial_landmarks": key_points
+            }
+
+class RandomCrop:
+    
+    def __init__(self, size: Union[int, Tuple[int, int]]):
+        if not isinstance(size, (int, tuple)):
+            raise ("Argument Size takes values of type int or tuple")
+            
+        self._size = int(size)
+        if isinstance(size, tuple):
+            if len(size) != 2:
+                raise ValueError("Size Tuple should have two elements- height and weight")
+        else:
+            self._size = (int(size), int(size))
+            
+    def __call__(self, sample):
+        image = sample["image"]
+        key_points = sample["facial_landmarks"]
+        current_height, current_weight = image.shape
+        
+        new_h, new_w = self._size
+        top = np.random.randint(0, current_height - new_h)
+        left = np.random.randint(0, current_weight - new_w)
+
+        return {
+            'image': image[top: top + new_h, left: left + new_w], 
+            'facial_landmarks': key_points - [left, top]
+        }
+
+    
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+    def __call__(self, sample):
+        image = sample["image"]
+        key_points = sample["facial_landmarks"]
+         
+        # if image has no grayscale color channel, add one
+        if(len(image.shape) == 2):
+            # add that third color dim
+            image = image.reshape(image.shape[0], image.shape[1], 1)
+            
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+        
+        return {
+            'image': torch.from_numpy(image),
+            'facial_landmarks': torch.from_numpy(key_points)
+        }
