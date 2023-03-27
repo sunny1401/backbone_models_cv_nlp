4509c07d0c130f173888f1239f13bcca1291201c
Sankranti Joshi <sankrantijoshi14@gmail.com>
2023-03-27 18:59:06 +0100

    Adding some cuda functions for ease of import

---
+++ src/utils/cuda.py
@@ -0,0 +1,14 @@
+from pynvml import (
+    nvmlInit, nvmlDeviceGetHandleByIndex, nvmlDeviceGetMemoryInfo
+)
+
+
+def print_gpu_utilization():
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+    info = nvmlDeviceGetMemoryInfo(handle)
+    print(f"GPU memory occupied: {info.used//1024**2} MB.")
+
+
+
+
